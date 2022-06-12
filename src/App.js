267e1b5980168d4fc3920eee86831c9248e96d38
sanaz-niamati <sanaz.niamati@yahoo.com>
2022-06-12 15:25:34 +0430

    create blob shape

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {Stage,Layer,Line} from "react-konva";
 
-function App() {
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Line
+              x={20}
+              y={200}
+              points={[145, 200,100,120,180,120]}
+              tension="0.5"
+              closed
+              stroke="black"
+              fill="black"
+              draggable
 
-export default App;
+          />
+        </Layer>
+      </Stage>
+  );
+};
